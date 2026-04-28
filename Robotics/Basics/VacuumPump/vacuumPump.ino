1f647c1891b6496f672c6450e665aa3cb7ecb9a2
Viktor Petrov <115278686+viktorptrv@users.noreply.github.com>
2026-04-28 21:43:30 +0300

    Add files via upload

---
+++ Robotics/Basics/VacuumPump/vacuumPump.ino
@@ -0,0 +1,14 @@
+int relay = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(relay, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(relay, HIGH);
+  delay(3000);
+  digitalWrite(relay, LOW);
+  delay(3000);
+}
