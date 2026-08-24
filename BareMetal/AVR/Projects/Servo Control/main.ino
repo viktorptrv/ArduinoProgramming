c30ae39fcf60c21d371c8ead6c5ed51193e54bdb
Viktor Petrov <115278686+viktorptrv@users.noreply.github.com>
2026-08-24 17:32:34 +0300

    Update main.ino

--- BareMetal/AVR/Projects/Servo Control/main.ino
+++ BareMetal/AVR/Projects/Servo Control/main.ino
@@ -10,7 +10,7 @@ static inline void init_timer1_servo(void);
 static inline void init_timer1_servo(void){
   // We have to setup TIMER1 to give a pulse every 20ms
   // We are going to use FASTPWM mode, counter max in ICR1
-  TCCR1A |= (1 << WGM11) | (1 << WGM10);                   // Set Fast PWM
+  TCCR1A |= (1 << WGM11);                   // Set Fast PWM
   TCCR1B |= (1 << WGM12) | (1 << WMG13);    // Set CTC
   TCCR1B |= (1 << CS11) | (1 << CS10);   // prescaler /16 only — NOT CS10 too
   ICR1 = 4999;             // 20000 ticks * 1µs = 20ms                         // Sets direct output on PB1
