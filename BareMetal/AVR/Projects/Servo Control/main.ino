2cfea954b485c1ea8a64df8073530b473aa8e477
Viktor Petrov <115278686+viktorptrv@users.noreply.github.com>
2026-08-24 17:11:55 +0300

    Update main.ino

--- BareMetal/AVR/Projects/Servo Control/main.ino
+++ BareMetal/AVR/Projects/Servo Control/main.ino
@@ -2,20 +2,31 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-#define PULSE_MIN 1000
-#define PULSE_MAX 2000
-#define PULSE_MID 1500
+#define PULSE_MIN 625
+#define PULSE_MAX 125
+#define PULSE_MID 350
 
-void init_timer1_servo(void);
-void init_timer1_servo(void){
+static inline void init_timer1_servo(void);
+static inline void init_timer1_servo(void){
   // We have to setup TIMER1 to give a pulse every 20ms
   // We are going to use FASTPWM mode, counter max in ICR1
   TCCR1A |= (1 << WGM11);                   // Set Fast PWM
   TCCR1B |= (1 << WGM12) | (1 << WGM13);    // Set CTC
-  TCCR1B |= (1 << CS10)                     // Set no prescalling (we count in microseconds)
-  ICR1 = 20000;                             // Sets direct output on PB1
+  TCCR1B |= (1 << CS11) | (1 << CS10);   // prescaler /16 only — NOT CS10 too
+  ICR1 = 4999;             // 20000 ticks * 1µs = 20ms                         // Sets direct output on PB1
   TCCR1A |= (1 << COM1A1);                  // Clear OC1A/OC1B on compare match, set OC1A/OC1B at BOTTOM (non-inverting mode)
   DDRB |= (1 << PB1);
+}
+
+static inline void showOff(void){
+  OCR1A = PULSE_MID;
+  _delay_ms(1500);
+  OCR1A = PULSE_MIN;
+  _delay_ms(1500);
+  OCR1A = PULSE_MAX;
+  _delay_ms(1500);
+  OCR1A = PULSE_MID;
+  _delay_ms(1500);
 }
 
 int main(void){
@@ -23,4 +34,10 @@ int main(void){
   uint16_t servoPulseLength;
   OCR1A = PULSE_MID;
   init_timer1_servo();
+  showOff();
+
+  while(1){
+    showOff();
+  }
 }
+
