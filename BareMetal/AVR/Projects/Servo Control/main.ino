5d03a399e1e2decf1e7c0efdd15d602a2c8fe807
Viktor Petrov <115278686+viktorptrv@users.noreply.github.com>
2026-08-24 21:34:38 +0300

    Update main.ino

--- BareMetal/AVR/Projects/Servo Control/main.ino
+++ BareMetal/AVR/Projects/Servo Control/main.ino
@@ -11,9 +11,9 @@ static inline void init_timer1_servo(void){
   // We have to setup TIMER1 to give a pulse every 20ms
   // We are going to use FASTPWM mode, counter max in ICR1
   TCCR1A |= (1 << WGM11);                   // Set Fast PWM
-  TCCR1B |= (1 << WGM12) | (1 << WMG13);    // Set CTC
-  TCCR1B |= (1 << CS11) | (1 << CS10);   // prescaler /16 only — NOT CS10 too
-  ICR1 = 4999;             // 20000 ticks * 1µs = 20ms                         // Sets direct output on PB1
+  TCCR1B |= (1 << WGM12) | (1 << WGM13);    // Set CTC
+  TCCR1B |= (1 << CS11) | (1 << CS10);      // prescaler /16 only — NOT CS10 too
+  ICR1 = 4999;                               // 20000 ticks * 1µs = 20ms                         // Sets direct output on PB1
   TCCR1A |= (1 << COM1A1);                  // Clear OC1A/OC1B on compare match, set OC1A/OC1B at BOTTOM (non-inverting mode)
   DDRB |= (1 << PB1);
 }
@@ -29,6 +29,11 @@ static inline void showOff(void){
   _delay_ms(1500);
 }
 
+static inline void moveServo(uint16_t pulse_width);
+static inline void moveServo(uint16_t pulse_width){
+  OCR1A = pulse_width;
+}
+
 int main(void){
   // Inits
   uint16_t servoPulseLength;
@@ -37,7 +42,9 @@ int main(void){
   showOff();
 
   while(1){
-    showOff();
+    for (uint16_t pos = PULSE_MAX; pos <= PULSE_MIN; pos+= 5){
+      moveServo(pos);
+      _delay_ms(1500);
+    }
   }
 }
-
